326a544ed5dd3691edea13bd070a9db71bced014
caguilarquesada-design <caguilarquesada@udesa.edu.ar>
2025-11-14 17:10:24 -0300

    hicimos el buscador de la pagina

--- js/index.js
+++ js/index.js
@@ -1,9 +1,21 @@
-/* Header */
+/* Form buscador */
 
-const buscador = document.querySelector(".buscador");
+let formHeader = document.querySelector(".buscar");
+let buscador = document.querySelector(".buscador");
 
-if (buscador == ""){
-    alert = "El campo se encuentra vacio, para realizar la busqueda, ingrese un producto";
-} else if (buscador.length < 3){
-    alert = "El producto ingresado es menor de tres caracteres, ingrese otro producto";
-} 
+formHeader.addEventListener("submit", function(evento) {
+    evento.preventDefault();
+
+    console.log(buscador)
+    console.log(buscador.value)
+
+    if(buscador.value == ""){
+        return alert("El campo se encuentra vacio, para realizar la busqueda, ingrese un producto");
+    }
+    if (buscador.value.length < 3) {
+        return alert("El producto ingresado es menor de tres caracteres, ingrese otro producto");
+    } 
+
+    localStorage.setItem("usuario", buscador.value)
+})
+
