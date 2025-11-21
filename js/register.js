459e306292cc0f4f5693b9a49080cb7922f7eac4
Franco-Andersson <fandersson@udesa.edu.ar>
2025-11-21 16:03:50 -0300

    hice correciones para qeu funcione el register y arregle errores

--- js/register.js
+++ js/register.js
@@ -8,61 +8,74 @@ let msgEmail = document.querySelector(".msg-email");
 let msgPass = document.querySelector(".msg-pass");
 let msgPass2 = document.querySelector(".msg-pass2")
 
-campoEmail.addEventListener('focus', function(){
-    if (msgEmail) msgEmail.textContent = "";
+campoEmail.addEventListener('focus', function () {
+    if (msgEmail) msgEmail.innerText = "";
 })
 
-campoEmail.addEventListener('blur',function(){
-    if(campoEmail.value == "" && msgEmail){
-        msgEmail.textContent ="El email es obligatorio";
+campoEmail.addEventListener('blur', function () {
+    if (campoEmail.value == "" && msgEmail) {
+        msgEmail.innerText = "El email es obligatorio";
     }
 })
 
-campoEmail.addEventListener('input', function(){
-    if (campoEmail.value != "" && msgEmail){
-        msgEmail.textContent ="";
+campoEmail.addEventListener('input', function () {
+    if (campoEmail.value != "" && msgEmail) {
+        msgEmail.innerText = "";
     }
 })
 
-campoPass.addEventListener('focus', function(){
-    if (msgPass) msgPass.textContent="";
+campoPass.addEventListener('focus', function () {
+    if (msgPass) msgPass.innerText = "";
 })
 
-campoPass.addEventListener('blur', function(){
-    if (campoPass.value == "" && msgPass){
-        msgPass.textContent = "la contraseña es obligatoria";
+campoPass.addEventListener('blur', function () {
+    if (campoPass.value == "" && msgPass) {
+        msgPass.innerText = "la contraseña es obligatoria";
     }
 })
 
-campoPass2.addEventListener('focus', function(){
-    if (msgPass2) msgPass2.textContent ="";
+campoPass2.addEventListener('focus', function () {
+    if (msgPass2) msgPass2.innerText = "";
 })
 
-campoPass2.addEventListener('blur', function(){
-    if (campoPass2.value == "" && msgPass2){
-        msgPass2.textContent = "repetir contraseña es un campo obligatorio";
+campoPass2.addEventListener('blur', function () {
+    if (campoPass2.value == "" && msgPass2) {
+        msgPass2.innerText = "es un campo obligatorio";
     }
 })
 
-campoPass2.addEventListener('input', function(){
-    if (campoPass2.value != "" && msgPass2){
-        msgPass2.textContent ="";
+campoPass2.addEventListener('input', function () {
+    if (campoPass2.value != "" && msgPass2) {
+        msgPass2.innerText = "";
     }
 })
 
-formulario.addEventListener('submit', function(event){
+formulario.addEventListener('submit', function (event) {
     event.preventDefault();
 
-    if(msgEmail && campoEmail.value =="") msgEmail.textContent ="El email es obligatorio.";
-    if(msgPass && campoPass.value =="") msgPass.textContent ="La contraseña es obigatoria";
-    if(msgPass2 && campoPass2.value =="") msgPass2.textContent ="Repetir contraseña es obligatrio";
-
-   if (campoPass2.value != "" && campoPass.value != "" && campoPass2.value != campoPass.value) {
-     return alert("las contraseñas no coinciden");
+    if (msgEmail && campoEmail.value == "") {
+       return msgEmail.innerText = "El email es obligatorio.";
+    }
+    if (msgPass && campoPass.value == "") {
+       return msgPass.innerText = "La contraseña es obigatoria";
     }
 
+    if (campoPass.value.length < 6) {
+        return alert("la contraseña debe tener minimo 6 caracteres");
+    }
+
+    if (msgPass2 && campoPass2.value == "") {
+        return msgPass2.innerText = "Repetir contraseña es obligatrio";
+    }
+
+    if (campoPass2.value != "" && campoPass.value != "" && campoPass2.value != campoPass.value) {
+         return alert("las contraseñas no coinciden");
+    }
+
+    else {
+        formulario.submit();
+    }
     localStorage.setItem("usuarioEmail", campoEmail.value);
-    
-    this.submit();
+
 })
 
--- js/saludo.js
+++ js/saludo.js
@@ -5,8 +5,10 @@ let parrafousuario = document.querySelector(".parrafocontainer")
 let menu = document.querySelector(".log");
 
 if (usuario !== null){
-
     navusuario.style.display = "block";
     menu.style.display = 'none'
     parrafousuario.innerText = `Bienvenido ${usuario}`
+} else if (usuario == null){
+    navusuario.style.display = "none";
+    menu.style.display = 'block';
 }
